949fcdfbecd0ce9f5ae8efcc9320236174701690
BilelMatallah <bilelmatallah@hotmail.com>
2023-02-23 17:20:34 +0000

    feat: comentarios añadidos

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const pronoun = ["the"];
 const adj = ["great", "big", "master", "ruthless"];
 const extension =[".com", ".net", ".org"];
 
+
+
+
+
+
+/*Función para generar los dominios*/
 nombre.forEach ((apodo) => {
     pronoun.forEach ((articulo) => {
         adj.forEach((mote) => {
             extension.forEach ((dom) => {
-                console.log (`${apodo}${articulo}${mote}${dom}`)});
+              /*Imprimimos la combinación de los elementos anteriores*/
+              console.log (`${apodo}${articulo}${mote}${dom}`)});
         });
     });   
 });
